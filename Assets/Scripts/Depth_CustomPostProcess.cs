8f0a7cd7186764c3b0b16af6718bc08f372795ab
nmxi <mikki1118_2@hotmail.co.jp>
2018-05-06 01:20:26 +0900

    create : depth catch cs

---
+++ Assets/Scripts/Depth_CustomPostProcess.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Depth_CustomPostProcess : MonoBehaviour {
+    public Material mat;
+    void Start () {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture dest){                
+        Graphics.Blit(source, dest, mat);
+    }
+}
